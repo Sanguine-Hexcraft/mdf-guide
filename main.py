9232b998ff2560a020a03f21ac9af7862d66aed6
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-01-05 15:06:36 -0700

    project init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mdf-guide!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/mdf/__main__.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("mdf-guide initialized")
+
+if __name__ == "__main__":
+    main()
