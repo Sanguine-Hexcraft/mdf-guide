13926ee32b8304e6eaf46cb40366f789d4e406de
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-01-05 20:31:17 -0700

    cleaned up logic and path

--- src/mdf/__main__.py
+++ src/mdf/__main__.py
@@ -3,24 +3,22 @@ import sys
 
 def main():
 
-    p = Path("/home/sanguine/workspace/mdf-guide/data/bands.csv")
+    p = Path("data/bands.csv")
     
     if len(sys.argv) < 2:
         print("Please use a search term, for example (uv run python -m mdf <search term>)")
         sys.exit(1)
 
-    search_term = sys.argv[1]
+    search_term = sys.argv[1].lower()
     results = []
 
-    with open(p, "r") as f:
-        contents = f.readlines()
+    with p.open() as f:
+        for line in f:
+            if search_term in line.lower():
+                results.append(line)
 
-    for line in contents:
-        
-        if search_term.lower() in line.lower():
-            results.append(line)
-
-    print(f"Search Results: {"".join(results)}")
+    print(f"Search Results:")
+    print("".join(results))
     # print(f"Contents: {contents}")
 
 
