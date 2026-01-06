e0b1fb37fc06424ff8a1b23eb78626065667e0c2
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-01-05 20:19:12 -0700

    simple search feature via system arguments in the commandline

--- src/mdf/__main__.py
+++ src/mdf/__main__.py
@@ -1,5 +1,28 @@
-def main() -> None:
-    print("mdf-guide initialized")
+from pathlib import Path
+import sys
+
+def main():
+
+    p = Path("/home/sanguine/workspace/mdf-guide/data/bands.csv")
+    
+    if len(sys.argv) < 2:
+        print("Please use a search term, for example (uv run python -m mdf <search term>)")
+        sys.exit(1)
+
+    search_term = sys.argv[1]
+    results = []
+
+    with open(p, "r") as f:
+        contents = f.readlines()
+
+    for line in contents:
+        
+        if search_term.lower() in line.lower():
+            results.append(line)
+
+    print(f"Search Results: {"".join(results)}")
+    # print(f"Contents: {contents}")
+
 
 if __name__ == "__main__":
     main()
